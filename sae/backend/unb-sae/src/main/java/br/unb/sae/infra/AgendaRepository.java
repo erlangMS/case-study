84331b6fcf7a548b84d7b0dc567ac309af1d479f
Vanderlei <vanderlei.jandir@gmail.com>
2015-12-03 15:27:15 -0200

    Criacao de alguns métodos que possam ser usados no agendamento das entrevistas dos alunos.

--- sae/backend/unb-sae/src/main/java/br/unb/sae/infra/AgendaRepository.java
+++ sae/backend/unb-sae/src/main/java/br/unb/sae/infra/AgendaRepository.java
@@ -25,10 +25,26 @@ public class AgendaRepository extends EmsRepository<Agenda>{
 		return saeContext;
 	}
 
-	public List<Agenda> pesquisar(String atendimento, String periodo, String dataInicio, String dataFim,
-			String horaInicio, String horaFim) {
-		// TODO Auto-generated method stub
-		return null;
+	@SuppressWarnings("unchecked")
+	public List<Agenda> pesquisarPorPeriodo(String dataInicio, String dataFim) {
+		StringBuilder SQL = new StringBuilder("Select a from Agenda a WHERE");
+		SQL.append("dataInicio = :dataInicio AND dataFim = :dataFim");
+		return getEntityManager().createQuery(SQL.toString())
+		.setParameter("dataInicio", dataInicio)
+		.setParameter("dataFim", dataFim)
+		.getResultList();
 	}
 
+	
+	@SuppressWarnings("unchecked")
+	public List<Agenda> pesquisarPorCampusAndSemestreAno(Integer campus,String semestreAno ) {
+		StringBuilder SQL = new StringBuilder("Select a from Agenda a WHERE");
+		SQL.append("periodo = :periodo");
+		SQL.append("campus = :campus");
+		return getEntityManager().createQuery(SQL.toString())
+				.setParameter("campus", campus)
+				.setParameter("periodo", semestreAno)
+		.getResultList();
+	}
+	
 }
--- sae/backend/unb-sae/src/main/java/br/unb/sae/infra/AgendamentoRepository.java
+++ sae/backend/unb-sae/src/main/java/br/unb/sae/infra/AgendamentoRepository.java
@@ -1,16 +1,18 @@
 package br.unb.sae.infra;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.erlangms.EmsRepository;
-import br.unb.sae.model.Agenda;
+import br.unb.sae.model.Agendamento;
+import br.unb.sae.model.AlunoSae;
 
 @Stateless
-public class AgendamentoRepository extends EmsRepository<Agenda> {
+public class AgendamentoRepository extends EmsRepository<Agendamento> {
 	
 	@PersistenceContext(unitName = "service_context")
 	public EntityManager saeContext;
@@ -21,16 +23,44 @@ public class AgendamentoRepository extends EmsRepository<Agenda> {
 	}
 
 	@Override
-	public Class<Agenda> getClassOfModel() {
-		return Agenda.class;
+	public Class<Agendamento> getClassOfModel() {
+		return Agendamento.class;
 	}
 	
-	public int getQuantidadeAgendamentosMesmoHorario (Date dataHora) {
-		final String sql = "SELECT COUNT(a) FROM Agenda a WHERE a.dataHora = :pDataHora";
+	public int getQuantidadeAgendamentosMesmoHorario(Date dataHora) {
+		final String sql = "SELECT COUNT(ag) FROM Agendamento ag WHERE ag.agenda.dataInicio = :pDataHora";
 		return getEntityManager()
 			.createQuery(sql)
 			.setParameter("pDataHora", dataHora)
 			.getFirstResult();
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<Agendamento> pesquisarAgendamentoPorAlunoCPF(AlunoSae alunoSae){
+		final String sql = "SELECT ag FROM Agendamento ag WHERE ag.aluno.cpf = :cpf";
+		return getEntityManager()
+			.createQuery(sql)
+			.setParameter("cpf", alunoSae.getCpf())
+			.getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Agendamento> pesquisarAgendamentoPorAlunoPorParteNome(AlunoSae alunoSae){
+		final String sql = "SELECT ag FROM Agendamento ag WHERE ag.aluno.nome like :nomeParte";
+		return getEntityManager()
+			.createQuery(sql)
+			.setParameter("nomeParte", alunoSae.getNome()+"%")
+			.getResultList();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Agendamento> pesquisarAgendamentoDoDia(Date dataDoDia){
+		final String sql = "SELECT ag FROM Agendamento ag WHERE ag.agenda.dataInicio = :dataAutal";
+		return getEntityManager()
+			.createQuery(sql)
+			.setParameter("dataAutal", dataDoDia)
+			.getResultList();
 	}
 
 }
