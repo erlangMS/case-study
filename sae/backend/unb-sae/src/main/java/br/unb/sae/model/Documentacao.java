8568f88d3e798ecd014891974b5aaa4541427011
evertonagilar <evertonagilar@gmail.com>
2015-12-08 15:35:07 -0200

    Pequenos ajustes

--- sae/backend/unb-sae/src/main/java/br/unb/sae/model/Documentacao.java
+++ sae/backend/unb-sae/src/main/java/br/unb/sae/model/Documentacao.java
@@ -1,16 +1,12 @@
 package br.unb.sae.model;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import br.erlangms.EmsUtil;
